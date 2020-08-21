a6a254a3d14b3eda76064e7bf112d854c6ca9dbf
kryptn36 <51272147+kryptn36@users.noreply.github.com>
2020-08-21 15:28:52 +0200

    :pencil: Use CustomSlider for AudioProgress

--- app/components/player/parts/progress/AudioProgress.tsx
+++ app/components/player/parts/progress/AudioProgress.tsx
@@ -1,64 +1,16 @@
-import React, { useState, useRef } from 'react';
+import CustomSlider from 'components/extra/customslider/CustomSlider';
+import React from 'react';
 import styles from './AudioProgress.scss';
-import useWindowEvent from './WindowEventHook';
 
 export default function AudioProgress() {
-  const [width, setWidth] = useState(200);
-  const [isInDrag, setIsInDrag] = useState(false);
-  const timelineRef = useRef<HTMLDivElement>(null);
-  const progressRef = useRef<HTMLDivElement>(null);
-
-  function moveProgress(x: number) {
-    if (timelineRef.current && progressRef.current) {
-      const newWidth = x - progressRef.current.getBoundingClientRect().left;
-      if (newWidth >= 0 && newWidth <= timelineRef.current.offsetWidth) {
-        setWidth(newWidth);
-      }
-    }
-  }
-
-  function mouseUp(event) {
-    event.preventDefault();
-
-    setIsInDrag(false);
-    window.removeEventListener('mouseup', mouseUp);
-  }
-
-  function onMouseDown(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-    event.preventDefault();
-    moveProgress(event.clientX);
-    setIsInDrag(true);
-  }
-
-  function onMouseMove(event: MouseEvent) {
-    event.preventDefault();
-    if (isInDrag) {
-      moveProgress(event.clientX);
-    } else {
-      // Unregister to prevent unneeded calls.
-      window.removeEventListener('mousemove', onMouseMove);
-    }
-  }
-
-  useWindowEvent([
-    { eventName: 'mousemove', callback: onMouseMove },
-    { eventName: 'mouseup', callback: mouseUp },
-  ]);
-
   return (
     <div>
-      <div
-        ref={timelineRef}
-        onMouseDown={onMouseDown}
-        className={styles['audio-timeline']}
-      >
-        <div
-          ref={progressRef}
-          onMouseDown={onMouseDown}
-          className={styles['audio-timeline--progress']}
-          style={{ width: width + 'px' }}
-        ></div>
-      </div>
+      <CustomSlider
+        className={styles.audioProgress}
+        thumbImageUrl=""
+        leftColor="#88b617"
+        rightColor="#3c3d41"
+      />
     </div>
   );
 }
